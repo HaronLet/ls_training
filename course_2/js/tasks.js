0dad2af2971b9075a70f43de873a301275b4cb53
Агапов Михаил <muhaxt@yandex.ru>
2020-07-05 20:07:38 +0300

    Выполнил задания для 4 недели

--- course_2/js/tasks.js
+++ course_2/js/tasks.js
@@ -4,22 +4,18 @@ const outString = document.querySelector("#out_task_1");
 const btn = document.querySelector("#btn_1");
 
 btn.addEventListener('click', function(event) {
-
   outString.value = getVowels(inString.value);
   console.log(outString.value);
 });
 
 function getVowels(str) {
-
   const arr = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
   var getString = "";
   
   str = str.toLowerCase();
 
   for(let i = 0; i < str.length; i++) {
-
     for(let n = 0; n < arr.length; n++) {
-
       if (str[i].includes(arr[n])) {
         getString = getString + arr[n];
       }
@@ -50,10 +46,8 @@ inArrStaff_2.forEach(element => {
   inStaff_2.value = inStaff_2.value + element.name + ", зарплата: " + element.salary + "\n";
 });
 
-inBtn_2.addEventListener('click', function(event) {
-  
-  inArrStaff_2.push({name: inName_2.value, salary: inSalary_2.value});
-  
+inBtn_2.addEventListener('click', function(event) {  
+  inArrStaff_2.push({name: inName_2.value, salary: inSalary_2.value});  
   inStaff_2.value = inStaff_2.value + inArrStaff_2[i].name + ", зарплата: " + inArrStaff_2[i].salary + "\n";
 
   i++;
@@ -62,14 +56,11 @@ inBtn_2.addEventListener('click', function(event) {
   inSalary_2.value = "";
 });
 
-outBtn_2.addEventListener('click', function(event) {
-  
+outBtn_2.addEventListener('click', function(event) {  
   outArrStaff_2 = [];
 
   inArrStaff_2.forEach(function(element) {
-
     if(element.salary > 1000) {
-
       outArrStaff_2.push(element.name);
     }
   });
@@ -84,14 +75,11 @@ const inPath_3 = document.querySelector("#in_task_3");
 const out_3 = document.querySelector("#out_task_3");
 const btn_3 = document.querySelector("#btn_3");
 
-btn_3.addEventListener('click', function(event) {
-  
+btn_3.addEventListener('click', function(event) {  
   if(inPath_3.value.slice(-5) == ".html") {
-
     out_3.value = "Файл имеет расширение .html";
     console.log(inPath_3.value.slice(-5) == ".html");
   } else {
-
     out_3.value = "Файл не имеет расширение .html";
     console.log(inPath_3.value.slice(-5) == ".html");
   }
@@ -110,28 +98,22 @@ var endMixedArray = [];
 
 inArr_4.value = beginMixedArray;
 
-inBtn_4.addEventListener('click', function(event) {
-  
+inBtn_4.addEventListener('click', function(event) {  
   beginMixedArray.push(inElemArr_4.value);
-
   inElemArr_4.value = "";
   inArr_4.value = beginMixedArray;
 });
 
-outBtn_4.addEventListener('click', function(event) {
-  
+outBtn_4.addEventListener('click', function(event) {  
   endMixedArray = [];
 
-  beginMixedArray.forEach(function(element) {
-    
+  beginMixedArray.forEach(function(element) {    
     if(element % 2 == 0) {
-
       endMixedArray.push(element);
     }    
   });
 
   outArr_4.value = endMixedArray;
-
   console.log(outArr_4.value);
 });
 
@@ -147,24 +129,18 @@ const step = -50;
 var counterSlider = 0;
 
 controlRight.addEventListener('click', function(event) {
-
   if(counterSlider == maxLeft) {
-
     event.preventDefault();
   } else {
-
     counterSlider = counterSlider + step;
     sliderList.style.left = counterSlider + "px";
   }
 });
 
 controlLeft.addEventListener('click', function(event) {
-
   if(counterSlider == minLeft) {
-
     event.preventDefault();
   } else {
-
     counterSlider = counterSlider - step;
     sliderList.style.left = counterSlider + "px";
   }
@@ -176,12 +152,10 @@ const buttonOpen = document.querySelector("#btn_open");
 const modalContainer = document.querySelector("#modal-container");
 
 buttonOpen.addEventListener("click", function() {
-
   createOverlay("Hello, <b>world</b>!");
 });
 
 function createOverlay(content) {
-
   const overlayElement = document.createElement("div");
   overlayElement.classList.add("overlay");
   
@@ -198,20 +172,16 @@ function createOverlay(content) {
   modalElement.append(closeElement);
 
   overlayElement.addEventListener("click", function(event) {
-
     if(event.target == overlayElement) {
-
       deleteOverlay();
     }
   });
 
-  closeElement.addEventListener("click", function() {
-  
+  closeElement.addEventListener("click", function() {  
     deleteOverlay();
   });
 
-  function deleteOverlay() {
-    
+  function deleteOverlay() {    
     closeElement.remove();
     modalElement.remove();
     overlayElement.remove();
@@ -227,57 +197,42 @@ const containerElement = document.querySelector(".container");
 var controlChange = false;
 
 counterElement.addEventListener("change", function(event) {
-
   createNewElement(counterElement.value);
 });
 
-function createNewElement(number) {
-  
+function createNewElement(number) {  
   let NewElements = "";
 
   for (let i = 0; i < number; i++) {
-
     const NewElement = document.createElement("div");
     NewElement.className = "new_element";
     NewElement.textContent = i + 1;
-
     NewElements = NewElements + NewElement.outerHTML;
   }
 
   containerElement.innerHTML = NewElements;
 };
 
-colorElement.addEventListener("change", function(event) {
-  
+colorElement.addEventListener("change", function(event) {  
   controlChange = !controlChange;
 
   const color = event.target.value;
   const arrNewElements = containerElement.querySelectorAll(".new_element");
 
   for (let i = 0; i < arrNewElements.length; i++) {
-
     const numberNewElement = i + 1;
     const NewElement = arrNewElements[i];
 
     if (controlChange) {
-
       if (numberNewElement % 2 != 0) {
-
         NewElement.style.backgroundColor = color;
-
       } else {
-
         NewElement.style.backgroundColor = "#fff";
-
       }
     } else {
-
       if (numberNewElement % 2 == 0) {
-
         NewElement.style.backgroundColor = color;
-
       } else {
-
         NewElement.style.backgroundColor = "#fff";
       }
     }
